a6a6dd805274d4f35615b5dc62140f19abfabc03
dhylan01 <dhylan01@users.noreply.github.com>
2022-01-19 16:11:54 -0800

    bug fixes
    The program looks just for brackets and parentheses to indicate a link, not an actual link tag. We can add an if statement so that it only adds text with a period and no spaces.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,10 +17,17 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (markdown.substring(openParen + 1, closeParen).contains(".")
+                    && !markdown.substring(openParen + 1, closeParen).contains(" ")) {
+                System.out.println("condition is working");
+                System.out.println(currentIndex);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+            }
             currentIndex = closeParen + 1;
             currentIndexChanges++;
         }
+
         System.out.println("Current index changes this many times: " + currentIndexChanges);
         return toReturn;
     }
