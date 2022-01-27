cd83bbb6661282a8267efd58cd654b2d347eeb3c
dhylan01 <dhylan01@users.noreply.github.com>
2022-01-26 16:39:21 -0800

    start markdown test

--- newMarkDownParse/MarkdownParseTest.java
+++ newMarkDownParse/MarkdownParseTest.java
@@ -1,9 +1,18 @@
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(5 + 10 , 15);
+        assertEquals(5 + 10, 15);
     }
+
+    @Test
+    public void MarkdownParse() {
+        assertEquals(MarkdownParse.getLinks("test-file.md"), List.of("https://something.com", "some-page.html"));
+    }
+
 }
