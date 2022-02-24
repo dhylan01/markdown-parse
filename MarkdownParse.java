70013b211c764bee2ea133d7e445d23bec33c4ae
dhylan01 <dhylan01@gmail.com>
2022-02-24 12:38:00 -0800

    update for start of lap report 8 w/ tests

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,3 +1,4 @@
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,31 +11,30 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        boolean foundParen = true;
+        while (currentIndex < markdown.length() && foundParen) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            // check if there even is an openParen and closeParen before
-            // creating a substring. Also check closeParen is before openParen
-            if (openParen != -1 && closeParen > openParen && closeParen != -1) {
+            if (!markdown.substring(closeParen + 1, markdown.length()).contains(")")) {
+                foundParen = false;
+            }
+            if (markdown.substring(openParen + 1, closeParen).contains(".") &&
+                    !markdown.substring(openParen + 1, closeParen).contains(" ") &&
+                    (nextOpenBracket == 0 ||
+                            !markdown.substring(nextOpenBracket - 1, nextOpenBracket).contains("!"))) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
-            } else {
-                currentIndex = nextCloseBracket + 1;
             }
-            // if there is no open bracket from the current index
-            // update currentIndex so that it's greater than markdown.length()
-            // in order to escape the while loop
-            if (markdown.indexOf("[", currentIndex) == -1 || markdown.length() < 4) {
-                currentIndex = markdown.length() + 1;
-            }
+            currentIndex = closeParen + 1;
         }
+
         return toReturn;
     }
+
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,22 +1,49 @@
-import static org.junit.Assert.*;
-import org.junit.*;
 
-import java.io.IOException;
+import static org.junit.Assert.*;
 import java.nio.file.Files;
+import java.util.*;
 import java.nio.file.Path;
-import java.util.List;
+import java.io.IOException;
+
+import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 2);
+        assertEquals(5 + 10, 15);
     }
 
     @Test
-    public void testFile1() throws IOException {
-        String contents = Files.readString(Path.of("test-file.md"));
-        List<String> expect = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void MarkdownParse() {
+        assertEquals(MarkdownParse.getLinks("test-file.md"), List.of("https://something.com", "some-page.html"));
+    }
+
+    @Test
+    public void testMarkDownParseTestFile() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+    }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        Path fileName = Path.of("D:/markdown-parse/newMarkDownParse/snippet1.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("`google.com", "google.com", "ucsd.edu"));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        Path fileName = Path.of("D:/markdown-parse/newMarkDownParse/snippet2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("a.com(())", "example.com"));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        Path fileName = Path.of("D:/markdown-parse/newMarkDownParse/snippet3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("https://ucsd-cse15l-w22.github.io/"));
     }
 
 }
