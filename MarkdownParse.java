ebce10992f4891035a125d352606b76b203883ac
dhylan01 <77525379+dhylan01@users.noreply.github.com>
2022-03-02 16:28:30 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -52,9 +52,8 @@ public class MarkdownParse {
     }
 
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
+        File f = new File(args[0]);
+        Map<String, List<String>>links = getLinks(f);
         System.out.println(links);
     }
 }
